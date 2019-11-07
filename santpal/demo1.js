8461096a15c09f461e46a8b308b24e72cb4c64c6
jamesjoel <james.joelj@gmail.com>
2019-11-07 14:00:42 +0530

    santpal day 1 for JS

---
+++ santpal/demo1.js
@@ -0,0 +1,70 @@
+// --------- Variables -------------
+
+
+/*
+    Data Types 
+        1. Number (Int/Float)
+        2. Boolean (true/false)
+        3. String ("", '')
+        
+        4. Array
+        5. Object
+        6. JSON
+        7. Null
+        8. Undefined
+
+
+
+
+*/
+var a = "ROHIT"; // String
+var b = 100; // Number (int)
+var c = 74.63; // Number (float)
+var d = false; // boolean
+
+
+/*
+  ----------- Operators ----------
+    
+    A. Arthimetic Ope.
+        i.          +           Sum
+        ii.         -           Sub
+        iii.        *           Muti
+        iv.         /           Divid
+        v.          %           Modules  var a = 27%4 -- 3;
+
+    B. Assinment Ope.
+        i.          =           var a = 100
+        ii.         +=
+        iii.        -=
+        iv.         *=
+        v.          /=
+        vi.         %=
+
+    
+    C. Comapiression Ope.
+
+        i.          ==          is equal
+        ii.         !=          is not equal
+        iii.        <           less then
+        iv.         >           greatar then
+        v.          <=          less then equal
+        vi.         >=          greater then equal
+        vii.        ===         value and type equal
+        viii.       !==         value but type is not equal
+    
+    D. Logical Ope
+
+        i.          &&          AND
+        ii.         ||          OR
+        iii.        !           Is Not
+    
+    E. String Op
+
+        i.          +           Concatenation
+        ii.         +=          Concatenation
+
+*/
+
+
+
