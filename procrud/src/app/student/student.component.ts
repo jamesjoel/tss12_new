3b6c830f642a83d24db8321b2ec4d0c7f3f3fb14
jamesjoel <james.joelj@gmail.com>
2019-11-15 14:24:02 +0530

    CRUD in angular

--- procrud/src/app/student/student.component.ts
+++ procrud/src/app/student/student.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class StudentComponent implements OnInit {
 
   student={
+    id: null,
     name : "",
     age : null,
     fee : null
@@ -17,32 +18,96 @@ export class StudentComponent implements OnInit {
 
   studentArr=[
     {
+      id : 1,
       name : "rohit",
       age : 25,
       fee : 5000
     },
     {
+      id : 2,
       name: "jaya",
       age: 25,
       fee: 7000
+    },
+    {
+      id : 3,
+      name : "rajat",
+      age : 25,
+      fee : 6500
+    },
+    {
+      id : 4,
+      name : "gaurav",
+      age : 30,
+      fee : 4500
     }
   ]
 
 
+  oneStudent={
+    id : null,
+    name : "",
+    age : null,
+    fee : null
+  };
+  n;
 
   constructor() { }
 
   ngOnInit() {
   }
+  clearData(){
+    this.student = {
+      id : null,
+      name: "",
+      age: null,
+      fee: null
+    }
+  }
 
   save(){
     // console.log(this.student);
-    this.studentArr.push(this.student);
-    this.student={
-      name : "",
-      age : null,
-      fee : null
+    if(this.student.id)
+    {
+      // for(var i=0; i<this.studentArr.length; i++)
+      // {
+      //   if(this.studentArr[i].id==this.student.id)
+      //   {
+      //     this.studentArr[i]=this.student;
+      //   }
+      // }
+      this.studentArr[this.n]=this.student;
     }
+    else
+    {
+      this.student.id = this.studentArr.length + 1;
+      this.studentArr.push(this.student);
+    }
+    this.clearData();
+
+
+
+    
+    // this.student={
+    //   name : "",
+    //   age : null,
+    //   fee : null
+    // }
+  }
+
+  askDelete(a){
+    // console.log(a);
+    this.oneStudent=a;
+  }
+  delete(){
+    let n = this.studentArr.indexOf(this.oneStudent);
+    this.studentArr.splice(n, 1);
+  }
+  askEdit(a){
+    this.n = this.studentArr.indexOf(a);
+    // this.student=a;
+    this.student = Object.assign({}, a);
+    // Object.assign is used for break 2 way data binding
   }
 
 }
