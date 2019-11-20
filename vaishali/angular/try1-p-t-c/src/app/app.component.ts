5a6a32678afa3301decb1230c51f842e0922b9e3
vaischh21 <56518999+vaischh21@users.noreply.github.com>
2019-11-20 12:47:13 +0530

    changes by vashali

---
+++ vaishali/angular/try1-p-t-c/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'try1-p-t-c';
+  pName="click",
+}
